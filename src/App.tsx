96f7c553ca10c620484b0924975dcd01c2551540
Kilian <dankestmirror@hotmail.es>
2026-04-17 17:44:59 +0100

    ite 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-
+import { studentsData } from './students'
 
 import './App.css'
+import { useState } from 'react'
 
 function App() {
-
+const [students, setStudents] = useState(studentsData)
 
   return (
     <>
+      <div className='main'>
+        {students.map((student) => {
+          return(
+            <div className='student-card'>
+              <img src={student.image} alt="" />
+              <h3>{student.fullName}</h3>
+              <p>Programa: {student.program}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   )
 }
