9f6d1cc788b086772ffb0a2fd04ce43c94d7d228
Kilian <dankestmirror@hotmail.es>
2026-04-17 18:25:11 +0100

    install Tailwind and style basic

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,14 @@ const [students, setStudents] = useState(studentsData)
 
   return (
     <>
-      <div className='main'>
+      <div className='flex justify-center gap-6 m-5'>
         {students.map((student) => {
           return(
-            <div className='student-card'>
-              <img src={student.image} alt="" />
-              <h3>{student.fullName}</h3>
-              <p>Programa: {student.program}</p>
+            <div className=' p-2 bg-slate-100 flex rounded-md place-items-center  transition-all duration-500 hover:scale-105 hover:shadow-2xl cursor-pointer' >
+              <img className=' rounded-full border-blue-300 border-2' src={student.image} alt="" />
+              <div className='w-40'>
+              <h3 className=' text-blue-600 font-bold'>{student.fullName}</h3>
+              <p >Programa: {student.program}</p></div>
             </div>
           );
         })}
