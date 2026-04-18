c9afad500ef0f5b5430fdaa839706d74b8691724
Kilian <dankestmirror@hotmail.es>
2026-04-18 21:39:03 +0100

    Estructura basica de un form react

--- src/components/AddStudentForm.jsx
+++ src/components/AddStudentForm.jsx
@@ -1,11 +1,37 @@
+import { format } from "node:path";
 import { useState } from "react";
-
-export function AddStudentForm () {
+                            //recibimos la funcion del padre
+export function AddStudentForm (onAddstudent) {
     const [dataForm, setDataForm] = useState({
         fullName: '',
         image: 'url',
         phone: 0,
         email: '',
+        program: 'Web Dev',
         graduated: false
     })
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+
+        setDataForm({
+            ...dataForm,
+            [name]: type === 'checkbox' ? checked : value
+        })
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()//Evita el recargar de la pagina que hace por defecto
+
+        //Tengo que averiguar como hacer un id unico
+        const newStudent = {
+            ...dataForm,
+            //Añadimos el id unico
+        }
+        onAddstudent(newStudent)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            
+        </form>
+    );
 }
